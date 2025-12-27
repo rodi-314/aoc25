ba5c1663c3440acdfc5c95fa932148400705f1be
rodi-314 <rodi314159@gmail.com>
2025-12-28 02:26:09 +0800

    complete day 6

--- Day_6/day_6.py
+++ Day_6/day_6.py
@@ -1,11 +1,61 @@
 def part_1():
-    with open('day_6_test.txt', 'r') as input_file:
-        pass
+    with open('day_6.txt', 'r') as input_file:
+        grid = [line.split() for line in input_file.read().split('\n')]
+        total = 0
+        for col in range(len(grid[0])):
+            if grid[-1][col] == '+':
+                curr_total = 0
+                for row in grid[:-1]:
+                    curr_total += int(row[col])
+            else:
+                curr_total = 1
+                for row in grid[:-1]:
+                    curr_total *= int(row[col])
+
+            total += curr_total
+
+        print(f'Part 1: Grand total = {total}')
+
+
+def get_max_row_len(grid):
+    max_row_len = 0
+    for row in grid:
+        max_row_len = max(max_row_len, len(row))
+
+    return max_row_len
 
 
 def part_2():
-    with open('day_6_test.txt', 'r') as input_file:
-        pass
+    with open('day_6.txt', 'r') as input_file:
+        grid = input_file.read().split('\n')
+        total = 0
+        for col in range(get_max_row_len(grid)):
+            # Check operation
+            if col < len(grid[-1]) and grid[-1][col] == '+':
+                curr_total = 0
+                is_add = True
+            elif col < len(grid[-1]) and grid[-1][col] == '*':
+                curr_total = 1
+                is_add = False
+
+            # Form number
+            number = ''
+            for row in grid[:-1]:
+                if col < len(row) and row[col] != ' ':
+                    number += row[col]
+
+            # Perform operation
+            if number:
+                if is_add:
+                    curr_total += int(number)
+                else:
+                    curr_total *= int(number)
+            else:
+                total += curr_total
+
+        total += curr_total
+
+        print(f'Part 2: Grand total = {total}')
 
 
 def main():
